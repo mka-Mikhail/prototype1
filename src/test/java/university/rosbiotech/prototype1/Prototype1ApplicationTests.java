2cfe246ad3f4103efae19bbd2b938fa217a58476
mka-Mikhail <mixailrojnow@yandex.ru>
2023-03-09 23:51:19 +0300

    Initial commit

---
+++ src/test/java/university/rosbiotech/prototype1/Prototype1ApplicationTests.java
@@ -0,0 +1,13 @@
+package university.rosbiotech.prototype1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prototype1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
